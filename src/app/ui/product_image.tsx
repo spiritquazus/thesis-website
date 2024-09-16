aa696f59d6bb78e4e754fa918f248fa9b0668fbd
DXM <kimisaac010@gmail.com>
2024-09-16 16:00:41 +0900

    suspense wrapper fix 6 test

--- src/app/ui/product_image.tsx
+++ src/app/ui/product_image.tsx
@@ -7,8 +7,6 @@ export default function ProductImage({ accessType }: { accessType: string | null
     
 
     return (
-        <Suspense fallback={<div>Loading product image...</div>}>
             <img src={`${accessType=="h"?"./img/bottleH.webp":"./img/bottleL.webp"}`} className={styles.imageMain}></img>
-        </Suspense>
     )
 }
