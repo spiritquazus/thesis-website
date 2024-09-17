b8d92b490d8bf5ff61756796e10d1c539282105c
DXM <kimisaac010@gmail.com>
2024-09-18 01:56:45 +0900

    fixed table references 3

--- src/app/api/userUpdate/route.tsx
+++ src/app/api/userUpdate/route.tsx
@@ -2,31 +2,34 @@ import { NextResponse } from 'next/server';
 import { createPool } from '@vercel/postgres';
 
 export async function POST(req: Request) {
-    try {
-      const { id, startTime, endTime, email} = await req.json();
-      const pool = createPool()
-      const client = await pool.connect()
+  try {
+      const { id, startTime, endTime, email } = await req.json();
+      const pool = createPool();
+      const client = await pool.connect();
 
       if (email) {
-        await client.query(
-          'UPDATE users SET email = $1 WHERE id = $3',
-          [email, id]
-        )
-      }
-      if (endTime && startTime) {
-        const totalTime = (endTime - startTime) / 1000;
-        await client.query(
-          'UPDATE users SET total_time  = $1, end_time = $2 WHERE id = $3',
-          [totalTime, endTime, id]
-        )
+          // Update email if present
+          await client.query(
+              'UPDATE survey_users SET email = $1 WHERE id = $2',
+              [email, id]
+          );
       }
 
-      client.release()
-      return NextResponse.json({ message: 'User updated successfully' })
-    } catch (error) {
+      if (endTime && startTime) {
+          // Update total_time and end_time if both are present
+          const totalTime = (endTime - startTime) / 1000;
+          await client.query(
+              'UPDATE survey_users SET total_time = $1, end_time = $2 WHERE id = $3',
+              [totalTime, endTime, id]
+          );
+      }
+
+      client.release();
+      return NextResponse.json({ message: 'User updated successfully' });
+  } catch (error) {
       console.error('Error updating user:', error);
-      return NextResponse.json({ message: 'Database update failed', error }, { status: 500 })
-    }
+      return NextResponse.json({ message: 'Database update failed', error }, { status: 500 });
+  }
 }
 
 
