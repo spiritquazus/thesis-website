0f3f7b83236456455376e6cb8664fe14813b8994
DXM <kimisaac010@gmail.com>
2024-09-22 16:23:09 +0900

    fixing db logic again - Survey

--- src/app/api/userSurvey/route.tsx
+++ src/app/api/userSurvey/route.tsx
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
         //🚧stuff
         await client.query(`
             INSERT INTO survey 
-                (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10)
+                (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
             [userId, questions[0], questions[1], questions[2], questions[3], questions[4], questions[5], questions[6], questions[7], questions[8], questions[9], questions[10]]
         )
