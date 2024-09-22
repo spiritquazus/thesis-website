950b2ecd21a5e4c19ebcf951df6880c3df15ee8e
DXM <kimisaac010@gmail.com>
2024-09-22 16:33:34 +0900

    fixing double user error

--- src/app/api/userSurvey/route.tsx
+++ src/app/api/userSurvey/route.tsx
@@ -4,23 +4,30 @@ import { createPool } from '@vercel/postgres';
 
 export async function POST(req: Request) {
     try {
-        const { userId, questions,} = await req.json();
-        const pool = createPool()
-        const client = await pool.connect()
+        const { userId, questions } = await req.json();
+        const pool = createPool();
+        const client = await pool.connect();
 
-        //🚧stuff
+        // Check if a survey for this user already exists
+        const existingSurvey = await client.query('SELECT * FROM survey WHERE user_id = $1', [userId]);
+        
+        if (existingSurvey.rows.length > 0) {
+            return NextResponse.json({ message: 'Survey already submitted.' }, { status: 400 });
+        }
+
+        // Insert the new survey
         await client.query(`
             INSERT INTO survey 
                 (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
             [userId, questions[0], questions[1], questions[2], questions[3], questions[4], questions[5], questions[6], questions[7], questions[8], questions[9], questions[10]]
-        )
+        );
 
-        client.release()
-        return NextResponse.json({ message: 'User for survey successfully' })
+        client.release();
+        return NextResponse.json({ message: 'Survey submitted successfully.' });
     } catch (error) {
-        console.error('Error creating a new user:', error);
-        return NextResponse.json({ message: 'Survey Database update failed', error }, { status: 500 })
+        console.error('Error creating a new survey:', error);
+        return NextResponse.json({ message: 'Survey Database update failed', error }, { status: 500 });
     }
 }
   
