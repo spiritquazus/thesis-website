4365244207bab5318b0876535ea9b0d9eb533bf4
DXM <kimisaac010@gmail.com>
2024-09-22 16:39:08 +0900

    survey update logic

--- src/app/api/userSurvey/route.tsx
+++ src/app/api/userSurvey/route.tsx
@@ -12,21 +12,30 @@ export async function POST(req: Request) {
         const existingSurvey = await client.query('SELECT * FROM survey WHERE user_id = $1', [userId]);
         
         if (existingSurvey.rows.length > 0) {
-            return NextResponse.json({ message: 'Survey already submitted.' }, { status: 400 });
+            // Update the existing survey
+            await client.query(`
+                UPDATE survey
+                SET question1 = $1, question2 = $2, question3 = $3, question4 = $4, 
+                    question5 = $5, question6 = $6, question7 = $7, 
+                    question8 = $8, question9 = $9, question10 = $10, question11 = $11
+                WHERE user_id = $12`,
+                [...questions, userId]
+            );
+            return NextResponse.json({ message: 'Survey updated successfully.' });
+        } else {
+            // Insert the new survey
+            await client.query(`
+                INSERT INTO survey 
+                    (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+                [userId, ...questions]
+            );
+            return NextResponse.json({ message: 'Survey submitted successfully.' });
         }
 
-        // Insert the new survey
-        await client.query(`
-            INSERT INTO survey 
-                (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
-            [userId, questions[0], questions[1], questions[2], questions[3], questions[4], questions[5], questions[6], questions[7], questions[8], questions[9], questions[10]]
-        );
-
         client.release();
-        return NextResponse.json({ message: 'Survey submitted successfully.' });
     } catch (error) {
-        console.error('Error creating a new survey:', error);
+        console.error('Error processing survey:', error);
         return NextResponse.json({ message: 'Survey Database update failed', error }, { status: 500 });
     }
 }
